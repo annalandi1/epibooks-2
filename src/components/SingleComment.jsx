774aa71a13460689b5096aa6a07b3cc258f94891
annalandi1 <landianna.1106@gmail.com>
2024-11-28 17:02:30 +0100

    update

---
+++ src/components/SingleComment.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+import { ListGroup } from "react-bootstrap";
+
+class SingleComment extends Component {
+    render() {
+        return {
+          <ListGroup key={this.key} variant="dark">
+            <ListGroup.Item variant="dark" className="text-warning bg-dark">
+            </ListGroup.Item>
+          </ListGroup>
+        }
+    }
+}
+
+export default SingleComment
